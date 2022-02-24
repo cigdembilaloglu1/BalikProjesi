ff6a42aba0ad8da43b4f650be28dd5b1fd0620d0
acer-pc <acer-pc@ACER>
2022-02-24 13:55:34 +0300

    filletuser and controluser add

---
+++ BalikProjesi/FilletUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BalikProjesi
+{
+    public partial class FilletUser : Form
+    {
+        public FilletUser()
+        {
+            InitializeComponent();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
