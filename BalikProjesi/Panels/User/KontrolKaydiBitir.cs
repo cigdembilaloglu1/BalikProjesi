b267fddf2ca9e9478d32d49d648352b8f13d1df2
acer-pc <acer-pc@ACER>
2022-03-04 12:33:09 +0300

    kontrolkaydibitir controller,popup

--- BalikProjesi/Panels/User/KontrolKaydiBitir.cs
+++ BalikProjesi/Panels/User/KontrolKaydiBitir.cs
@@ -16,5 +16,63 @@ namespace BalikProjesi.Panels.User
         {
             InitializeComponent();
         }
+
+        private void BackBtn_Click(object sender, EventArgs e)
+        {
+            this.Controls.Clear();
+            ControlDashboard Ud = new ControlDashboard();
+            this.Controls.Add(Ud);
+            Ud.Dock = DockStyle.Fill;
+            Ud.Show();
+        }
+        //private void selections(object sender, EventArgs e)
+        //{
+        //    frmNumbers frm = new frmNumbers();
+
+        //    if (((Button)sender).Name == "HasatDefoBtn")
+        //    {
+        //        frm.formValue = HasatDefoTxt.Text;
+        //        frm.screenLabel.Text = HasatDefoTxt.Text;
+        //        frm.ShowDialog();
+        //        HasatDefoTxt.Text = frm.formValue.Trim();
+        //    }
+        //    else if (((Button)sender).Name == "KılcıkBtn")
+        //    {
+        //        frm.formValue = KılcıkTxt.Text;
+        //        frm.screenLabel.Text = KılcıkTxt.Text;
+        //        frm.ShowDialog();
+        //        KılcıkTxt.Text = frm.formValue.Trim();
+        //    }
+        //    else if (((Button)sender).Name == "BıcakDefoBtn")
+        //    {
+        //        frm.formValue = BıcakDefoTxt.Text;
+        //        frm.screenLabel.Text = BıcakDefoTxt.Text;
+        //        frm.ShowDialog();
+        //        BıcakDefoTxt.Text = frm.formValue.Trim();
+        //    }
+        //    else if (((Button)sender).Name == "DigerBtn")
+        //    {
+        //        frm.formValue = DigerTxt.Text;
+        //        frm.screenLabel.Text = DigerTxt.Text;
+        //        frm.ShowDialog();
+        //        DigerTxt.Text = frm.formValue.Trim();
+
+        //    }
+        //} 
+
+        private void RaporBtn_Click(object sender, EventArgs e)
+        {
+            PopUp popup = new PopUp();
+            //popup.label3.Text = HasatDefoBtn.Text + " adet hasat defo elde edildi.";
+            //popup.label4.Text = KılcıkBtn.Text + " adet kılçık elde edildi.";
+            //popup.label5.Text = BıcakDefoBtn.Text + " adet bıçak defo elde edildi.";
+            //popup.label6.Text = DigerBtn.Text + " adet diğer ürün elde edildi.";
+            popup.Show();
+        }
+   
+        private void HasatDefoBtn_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
